5ce374c44ae2676dcf81d8862b14f88255765302
Thomas Ingram <thomasingram@vertx.xyz>
2025-11-27 10:46:51 +1100

    Graph breadcrumbs are respected when navigating via them

--- Editor/BaseGraphWindow.cs
+++ Editor/BaseGraphWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
@@ -166,10 +167,21 @@ namespace GraphProcessor
 
 			if (_graphBreadcrumbs != null)
 			{
-				foreach (BaseGraph graph in _graphBreadcrumbs)
+				for (var i = 0; i < _graphBreadcrumbs.Count; i++)
 				{
+					BaseGraph graph = _graphBreadcrumbs[i];
 					if (graph == null) continue;
-					_breadcrumbs.PushItem(ObjectNames.NicifyVariableName(graph.name), () => InitializeGraph(graph));
+					BaseGraph[] parentCrumbs = _graphBreadcrumbs.Take(i).ToArray();
+					_breadcrumbs.PushItem(ObjectNames.NicifyVariableName(graph.name), () =>
+					{
+						InitializeGraph(graph);
+						if (parentCrumbs.Length > 0)
+						{
+							_graphBreadcrumbs.Clear();
+							_graphBreadcrumbs.AddRange(parentCrumbs);
+							UpdateBreadcrumbs();
+						}
+					});
 				}
 			}
 
