b3df6eb2e809887f4d0ef92edb00370c44fb1a8e
Thomas Ingram <thomasingram@vertx.xyz>
2026-03-12 21:17:15 +1100

    Fixed bugged sticky notes.

--- Editor/Views/StickyNoteView.cs
+++ Editor/Views/StickyNoteView.cs
@@ -1,54 +1,142 @@
 #if UNITY_2020_1_OR_NEWER
+using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
-using UnityEditor.UIElements;
+using UnityEditor.Search;
 using UnityEngine.UIElements;
 
 namespace GraphProcessor
 {
-    public class StickyNoteView : UnityEditor.Experimental.GraphView.StickyNote
-	{
-		public BaseGraphView	owner;
-		public StickyNote		note;
+	public sealed class StickyNoteView : UnityEditor.Experimental.GraphView.StickyNote
+    {
+        int m_ColorTheme;
 
-        Label                   titleLabel;
-        ColorField              colorField;
-
-        public StickyNoteView()
-        {
-            fontSize = StickyNoteFontSize.Small;
-            theme = StickyNoteTheme.Classic;
-		}
-
-		public void Initialize(BaseGraphView graphView, StickyNote note)
-		{
-			this.note = note;
-			owner = graphView;
-
-            this.Q<TextField>("title-field").RegisterCallback<ChangeEvent<string>>(e => {
-                note.title = e.newValue;
-            });
-            this.Q<TextField>("contents-field").RegisterCallback<ChangeEvent<string>>(e => {
-                note.content = e.newValue;
-            });
+        readonly DropdownField m_FontSizeDropdown;
         
-            title = note.title;
-            contents = note.content;
-            SetPosition(note.position);
-		}
+        public BaseGraphView	owner;
+        public StickyNote		note;
+        private bool initializing;
 
-		public override void SetPosition(Rect newPos)
-		{
-			base.SetPosition(newPos);
+        public StickyNoteView() : base("Packages/com.unity.visualeffectgraph/Editor/UIResources/uxml/VFXStickyNote.uxml", Vector2.zero)
+        {
+            this.styleSheets.Add(EditorGUIUtility.Load("StyleSheets/GraphView/Selectable.uss") as StyleSheet);
+            this.styleSheets.Add(EditorGUIUtility.Load("StyleSheets/GraphView/StickyNote.uss") as StyleSheet);
 
-            if (note != null)
-                note.position = newPos;
-		}
+            this.Q<Button>("swatch1").clicked += OnSwatch1;
+            this.Q<Button>("swatch2").clicked += OnSwatch2;
+            this.Q<Button>("swatch3").clicked += OnSwatch3;
+            this.Q<Button>("fitToText").clicked += OnClickFitToText;
+
+            m_FontSizeDropdown = this.Q<DropdownField>("fontSize");
+            m_FontSizeDropdown.choices = new List<string> { "Small", "Medium", "Large", "Huge" };
+            m_FontSizeDropdown.RegisterValueChangedCallback(OnFontSizeChanged);
+
+            styleSheets.Add(AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.unity.visualeffectgraph/Editor/UIResources/uss/VFXStickyNote.uss"));
+            this.capabilities |= Capabilities.Groupable;
+            this.RegisterCallback<StickyNoteChangeEvent>(OnUIChange);
+        }
+        
+        public void Initialize(BaseGraphView graphView, StickyNote note)
+        {
+	        initializing = true;
+	        this.note = note;
+	        owner = graphView;
+	        title = note.title;
+	        contents = note.content;
+	        SetPosition(note.position);
+	        SetTheme(note.theme);
+	        fontSize = (StickyNoteFontSize)note.fontSize;
+	        initializing = false;
+        }
+
+        void OnFontSizeChanged(ChangeEvent<string> evt)
+        {
+            if (Enum.TryParse<StickyNoteFontSize>(evt.newValue, out var newFontSize))
+            {
+                fontSize = newFontSize;
+                UpdateFontSize();
+            }
+        }
+
+        void OnClickFitToText()
+        {
+            FitText(false);
+        }
+
+        void OnSwatch1() => SetTheme(1);
+        void OnSwatch2() => SetTheme(2);
+        void OnSwatch3() => SetTheme(3);
+
+        void SetTheme(int swatch)
+        {
+            if (swatch != m_ColorTheme)
+            {
+                // Remove inline color for background
+                var nodeBorder = this.Q<VisualElement>("node-border");
+                nodeBorder.style.backgroundColor = new StyleColor(StyleKeyword.Null);
+                nodeBorder.style.borderTopColor = nodeBorder.style.borderRightColor = nodeBorder.style.borderBottomColor = nodeBorder.style.borderLeftColor = new StyleColor(StyleKeyword.Null);
+
+                // Remove inline color for text
+                this.Query<Label>().ForEach(x =>
+                {
+                    x.style.color = new StyleColor(StyleKeyword.Null);
+                });
+
+                RemoveFromClassList($"color-theme-{m_ColorTheme}");
+                note.theme = swatch;
+                m_ColorTheme = swatch;
+                AddToClassList($"color-theme-{m_ColorTheme}");
+            }
+        }
+
+        void OnUIChange(StickyNoteChangeEvent e)
+        {
+            switch (e.change)
+            {
+                case StickyNoteChange.Title:
+	                if (!initializing)
+		                owner.RegisterCompleteObjectUndo("Renamed sticky note");
+                    note.title = title;
+                    break;
+                case StickyNoteChange.Contents:
+	                if (!initializing)
+		                owner.RegisterCompleteObjectUndo("Changed sticky note contents");
+	                
+	                note.content = contents;
+                    break;
+                case StickyNoteChange.FontSize:
+	                if (!initializing)
+		                owner.RegisterCompleteObjectUndo("Changed sticky note font size");
+	                
+                    m_FontSizeDropdown.SetValueWithoutNotify(fontSize.ToString());
+                    UpdateFontSize();
+                    break;
+                case StickyNoteChange.Position:
+	                if (!initializing)
+		                owner.RegisterCompleteObjectUndo("Moved graph node");
+	                
+	                note.position = new Rect(resolvedStyle.left, resolvedStyle.top, style.width.value.value, style.height.value.value);
+                    break;
+            }
+        }
+
+        void UpdateFontSize()
+        {
+            var previousFontSizeString = note.fontSize;
+            note.fontSize = (int)fontSize;
+            if (fontSize > (StickyNoteFontSize)previousFontSizeString)
+            {
+                // Need to dispatch the fit text after the layout pass has properly taken the font size change
+                Dispatcher.Enqueue(() => FitText(false), 0.1f);
+            }
+        }
 
         public override void OnResized()
         {
-            note.position = layout;
+	        note.position = layout;
         }
-	}
+    }
 }
 #endif
