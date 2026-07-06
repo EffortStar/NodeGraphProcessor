e20b255378865945e77263724b32c32241bc85e7
Thomas Ingram <thomasingram@vertx.xyz>
2026-07-06 20:01:04 +1000

    Initial work compiling edge data delegates to IL.

---
+++ Editor/Compilation/GraphCompilation.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+using static GraphProcessor.GraphExpressionCompilation;
+
+namespace GraphProcessor
+{
+	public class ProcessGraphsForBuild : IPreprocessBuildWithContext
+	{
+		public int callbackOrder => 0;
+
+		public void OnPreprocessBuild(BuildCallbackContext ctx)
+		{
+			Debug.Log(nameof(ProcessGraphsForBuild));
+			CompileAllGraphs();
+		}
+
+		[MenuItem("Effort Star/Graphs/Compile Edge Push Functions")]
+		public static void CompileAllGraphs()
+		{
+			GraphCompilation compilation = new(
+				AssetDatabase.FindAssets("t:" + nameof(BaseGraph), new[] { "Assets" })
+					.Select(guid => AssetDatabase.LoadAssetAtPath<BaseGraph>(AssetDatabase.GUIDToAssetPath(guid)))
+					.Where(graph => graph != null)
+			);
+			compilation.Compile();
+		}
+	}
+
+	public sealed class GraphCompilation
+	{
+		private readonly Dictionary<EdgeKey, SerializableEdge> _edges = new();
+		private readonly int _graphCount;
+
+		public GraphCompilation(IEnumerable<BaseGraph> graphs)
+		{
+			_graphCount = 0;
+			foreach (BaseGraph graph in graphs)
+			{
+				// Don't compile subgraphs.
+				if (graph.IsSubgraph)
+				{
+					continue;
+				}
+
+				foreach (SerializableEdge edge in graph.edges)
+				{
+					if (edge.Key is { } key)
+					{
+						_edges.TryAdd(key, edge);
+					}
+				}
+
+				_graphCount++;
+			}
+		}
+
+		public void Compile()
+		{
+			Debug.Log($"Compiling {_edges.Count} FieldInfo->FieldInfo edges across {_graphCount} graphs.");
+
+			const string assemblyName = "Game.Graphs.Compiled";
+			AssemblyBuilder builder = AssemblyBuilder.DefineDynamicAssembly(
+				new AssemblyName(assemblyName),
+				AssemblyBuilderAccess.RunAndSave
+			);
+			ModuleBuilder module = builder.DefineDynamicModule($"{assemblyName}.dll");
+
+			TypeBuilder typeBuilder = module.DefineType(
+				"StaticEdgePushFunctions",
+				TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.Class | TypeAttributes.Abstract
+			);
+
+			Dictionary<int, MethodBuilder> keyToMethod = new();
+
+			FieldBuilder pushDelegatesField = typeBuilder.DefineField(
+				"s_pushDelegates",
+				typeof(Dictionary<int, PushDataDelegate>),
+				FieldAttributes.Static | FieldAttributes.Private
+			);
+
+			foreach ((EdgeKey key, SerializableEdge edge) in _edges)
+			{
+				Expression<PushDataDelegate> expression
+					= CreatePushDataExpression(edge);
+
+				NodeFieldPath from = edge.FromPort._fieldInfo.Path;
+				NodeFieldPath to = edge.ToPort._fieldInfo.Path;
+				string methodName = "Push_" +
+					$"<{GetTypeName(from.FieldInfo.DeclaringType)}_{from.FieldPath.Replace('.', '_')}>" +
+					"_To_" +
+					$"<{GetTypeName(to.FieldInfo.DeclaringType)}_{to.FieldPath.Replace('.', '_')}>";
+
+
+				MethodBuilder method = typeBuilder.DefineMethod(
+					methodName,
+					MethodAttributes.Private | MethodAttributes.Static,
+					typeof(void),
+					new[] { typeof(BaseNode), typeof(BaseNode) }
+				);
+
+				expression.CompileToMethod(method);
+				keyToMethod.Add(key.GetHashCode(), method);
+			}
+
+			/*BlockExpression delegateLookupBlock = CreatePushDataDelegateLookup();
+			ConstructorBuilder constructor = typeBuilder.DefineConstructor(
+				MethodAttributes.Static | MethodAttributes.Private,
+				CallingConventions.Standard,
+				Array.Empty<Type>()
+			);
+			Expression.Lambda(delegateLookupBlock).CompileToMethod(
+				constructor
+			);*/
+
+			MethodBuilder getMethod = typeBuilder.DefineMethod(
+				"Get",
+				MethodAttributes.Public | MethodAttributes.Static,
+				typeof(void),
+				new[] { typeof(EdgeKey) }
+			);
+			CreateGetMethod(getMethod);
+
+			typeBuilder.CreateType();
+
+			string assemblyFileName = $"{builder.GetName().Name}.dll";
+			builder.Save(assemblyFileName);
+			string destinationPath = Path.GetFullPath(Path.Combine("Library", "ScriptAssemblies", assemblyFileName));
+			File.Delete(destinationPath);
+			File.Move(assemblyFileName, destinationPath);
+			Debug.Log($"Finished compiling to \"{destinationPath}\".");
+			return;
+
+			static string GetTypeName(Type type)
+			{
+				if (!type.IsGenericType)
+				{
+					return type.Name;
+				}
+
+				string name = type.Name;
+				int iBacktick = name.IndexOf('`');
+				if (iBacktick > 0)
+				{
+					name = name.Remove(iBacktick);
+				}
+
+				name += "<";
+				Type[] typeParameters = type.GetGenericArguments();
+				for (var i = 0; i < typeParameters.Length; ++i)
+				{
+					string typeParamName = GetTypeName(typeParameters[i]);
+					name += i == 0 ? typeParamName : "_" + typeParamName;
+				}
+
+				name += ">";
+
+				return name;
+			}
+
+			BlockExpression CreatePushDataDelegateLookup()
+			{
+				// TODO figure out how to assign this
+				MemberExpression pushDelegateExpression = Expression.MakeMemberAccess(null, pushDelegatesField);
+				return Expression.Block(
+					keyToMethod.Select(args =>
+						{
+							(int key, var method) = args;
+							ConstantExpression keyConst = Expression.Constant(key, typeof(int));
+							UnaryExpression @delegate = Expression.Convert(Expression.MakeMemberAccess(null, method), typeof(PushDataDelegate));
+							return Expression.Call(pushDelegateExpression,
+								typeof(Dictionary<int, PushDataDelegate>).GetMethod(
+									nameof(Dictionary<int, PushDataDelegate>.Add),
+									BindingFlags.Public | BindingFlags.Instance
+								)!,
+								keyConst,
+								@delegate
+							);
+						}
+					)
+				);
+			}
+
+			void CreateGetMethod(MethodBuilder getMethod)
+			{
+				/*
+				  public static GraphExpressionCompilation.PushDataDelegate Get(int key)
+				  {
+				    GraphExpressionCompilation.PushDataDelegate pushDataDelegate;
+				    return StaticEdgePushFunctions.s_pushDelegates.TryGetValue(key, out pushDataDelegate) ? pushDataDelegate : (GraphExpressionCompilation.PushDataDelegate) null;
+				  }
+				 */
+
+				MemberExpression pushDelegateExpression = Expression.MakeMemberAccess(null, pushDelegatesField);
+
+				ParameterExpression keyParameter = Expression.Parameter(typeof(int), "key");
+				ParameterExpression outValue = Expression.Variable(typeof(PushDataDelegate), "value");
+
+				ConditionalExpression body = Expression.Condition(
+					test:
+					Expression.Call(
+						pushDelegateExpression,
+						typeof(Dictionary<int, PushDataDelegate>).GetMethod(
+							nameof(Dictionary<int, PushDataDelegate>.TryGetValue),
+							BindingFlags.Public | BindingFlags.Instance
+						)!,
+						keyParameter,
+						outValue
+					),
+					ifTrue: outValue,
+					ifFalse: Expression.Convert(Expression.Constant(null), typeof(PushDataDelegate))
+				);
+
+				Expression.Lambda(
+						Expression.Block(
+							variables: new[] { outValue },
+							body
+						),
+						keyParameter
+					)
+					.CompileToMethod(getMethod);
+			}
+		}
+	}
+}
---
+++ Runtime/Utils/GraphExpressionCompilation.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using UnityEngine;
+
+namespace GraphProcessor
+{
+	public readonly struct EdgeKey : IEquatable<EdgeKey>
+	{
+		public readonly FieldInfo FromRoot;
+		public readonly FieldInfo FromLeaf;
+		public readonly FieldInfo ToRoot;
+		public readonly FieldInfo ToLeaf;
+		private readonly int _hashCode;
+
+		public EdgeKey(
+			NodeFieldInformation from,
+			NodeFieldInformation to
+		)
+		{
+			FromRoot = from.Path.Root.FieldInfo;
+			ToRoot = to.Path.Root.FieldInfo;
+			FromLeaf = from.Path.FieldInfo;
+			ToLeaf = to.Path.FieldInfo;
+			_hashCode = HashCode.Combine(FromRoot, ToRoot, FromLeaf, ToLeaf);
+		}
+
+		public bool Equals(EdgeKey other) =>
+			FromRoot.Equals(other.FromRoot)
+			&& ToRoot.Equals(other.ToRoot)
+			&& FromLeaf.Equals(other.FromLeaf)
+			&& ToLeaf.Equals(other.ToLeaf);
+
+		public override bool Equals(object obj) => obj is EdgeKey other && Equals(other);
+
+		public override int GetHashCode() => _hashCode;
+
+		public static bool operator ==(EdgeKey left, EdgeKey right) => left.Equals(right);
+
+		public static bool operator !=(EdgeKey left, EdgeKey right) => !left.Equals(right);
+	}
+
+	public static class GraphExpressionCompilation
+	{
+		/// <summary>
+		/// Delegate to send the data from this port to another port connected by an edge.
+		/// </summary>
+		public delegate void PushDataDelegate(BaseNode from, BaseNode to);
+
+		private static readonly Dictionary<EdgeKey, PushDataDelegate> s_pushDataDelegates = new();
+		
+		public static PushDataDelegate GetPushDataDelegate(SerializableEdge edge, UnityEngine.Object context)
+		{
+			if (edge.Key is not { } edgeKey)
+			{
+				Debug.LogError($"[NodeGraph] Edges generated with {nameof(CustomPortBehaviorAttribute)} cannot be executed and must be removed from the graph during Realization. ({context})");
+				return null;
+			}
+
+			if (s_pushDataDelegates.TryGetValue(edgeKey, out var edgeDelegate))
+			{
+				return edgeDelegate;
+			}
+
+			try
+			{
+				edgeDelegate = CreatePushDataExpression(edge).Compile();
+			}
+			catch (Exception e)
+			{
+				Debug.LogException(e);
+				return null;
+			}
+
+			s_pushDataDelegates.Add(edgeKey, edgeDelegate);
+			return edgeDelegate;
+		}
+
+		private static readonly ParameterExpression[] s_params = new ParameterExpression[2];
+
+		public static Expression<PushDataDelegate> CreatePushDataExpression(SerializableEdge edge)
+		{
+			NodeFieldInformation fromFieldInfo = edge.FromPort._fieldInfo;
+			NodeFieldInformation toFieldInfo = edge.ToPort._fieldInfo;
+
+			// We keep slow checks inside the editor
+#if UNITY_EDITOR
+			if (!BaseGraph.TypesAreConnectable(fromFieldInfo.FieldType, toFieldInfo.FieldType))
+			{
+				Debug.LogError($"[NodeGraph] Can't convert from {fromFieldInfo.FieldType} to {toFieldInfo.FieldType}, " +
+					"you must specify a custom port function (i.e CustomPortInput or CustomPortOutput) for non-implicit conversions. " +
+					$" {edge.FromNode} -> {edge.ToNode}"
+				);
+				return null;
+			}
+#endif
+
+			if (toFieldInfo.Path.Parent != null)
+			{
+				// NOTE: This says "OutputObjectAttribute", because there isn't an InputObjectAttribute yet.
+				Debug.LogError($"[NodeProcessor] {nameof(OutputObjectAttribute)} is not yet supported on input ports.");
+				return null;
+			}
+
+			// Take the node in as a parameter.
+			ParameterExpression fromParam = Expression.Parameter(typeof(BaseNode), "from");
+			ParameterExpression toParam = Expression.Parameter(typeof(BaseNode), "to");
+			s_params[0] = fromParam;
+			s_params[1] = toParam;
+
+			// Convert the parameter to their real types.
+			NodeFieldPath fromLeaf = fromFieldInfo.Path;
+			NodeFieldPath fromRoot = fromLeaf.Root;
+			UnaryExpression fromConverted = Expression.Convert(fromParam, fromRoot.FieldInfo.DeclaringType!);
+
+			Expression fromParamField;
+			if (fromLeaf != fromRoot)
+			{
+				Stack<NodeFieldPath> pathStack = new();
+				for (NodeFieldPath from = fromLeaf; from != null; from = from.Parent)
+				{
+					pathStack.Push(from);
+				}
+
+				fromParamField = fromConverted;
+				while (pathStack.TryPop(out NodeFieldPath path))
+				{
+					fromParamField = Expression.Field(fromParamField, path.FieldInfo);
+				}
+			}
+			else
+			{
+				fromParamField = Expression.Field(fromConverted, fromLeaf.FieldInfo);
+			}
+
+			Type fromType = edge.FromPort.DisplayType ?? fromFieldInfo.FieldType;
+
+			UnaryExpression toConverted = Expression.Convert(toParam, toFieldInfo.Path.FieldInfo.DeclaringType!);
+			Expression toParamField = Expression.Field(toConverted, toFieldInfo.Path.FieldInfo);
+			Type toType = edge.ToPort.DisplayType ?? toFieldInfo.FieldType;
+
+			if (fromType != toType)
+			{
+				// If there is a user defined conversion function, then we call it
+				if (TypeAdapter.AreAssignable(fromType, toType))
+				{
+					// We add a cast in case there we're calling the conversion method with a base class parameter (like object)
+					UnaryExpression convertedParam = Expression.Convert(fromParamField, fromType);
+					fromParamField = Expression.Call(TypeAdapter.GetConversionMethod(fromType, toType), convertedParam);
+					// In case there is a custom port behavior in the output, then we need to re-cast to the base type because
+					// the conversion method return type is not always assignable directly:
+					fromParamField = Expression.Convert(fromParamField, toFieldInfo.FieldType);
+				}
+				else // otherwise we cast
+				{
+					fromParamField = Expression.Convert(fromParamField, toFieldInfo.FieldType);
+				}
+			}
+
+			BinaryExpression assign = Expression.Assign(toParamField, fromParamField);
+			return Expression.Lambda<PushDataDelegate>(assign, s_params);
+		}
+	}
+}
